4b58d023bad68b1730aec1b06c52c37266d5bac7
arulsatriaji5 <arulsatriaji5@gmail.com>
2026-04-08 15:26:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ function ubahMode() {
     let badanweb = document.body;
     badanweb.classList.toggle("dark-mode");
 }
+
+let daftarSkill = ["HTML", "CSS", "Java Script", "React", "Git", "Figma"];
+let kotakSkill = document.getElementById("skill");
+
+for (let i = 0; i < daftarSkill.length; i++) {
+    kotakSkill.innerHTML += "<div>" + daftarSkill[i] + "</div>"
+}
